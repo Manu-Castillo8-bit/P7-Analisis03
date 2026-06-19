2d2b438e627fd3b7ee0cc4e99b987d078f1fd446
Manuel <ernesto.ccastilloguerra2@gmail.com>
2026-06-18 21:27:52 -0600

    Nombres corregidos

---
+++ P7 Analisis03/Producto.xaml.cs
@@ -0,0 +1,9 @@
+namespace P7_Analisis03;
+
+public partial class Producto : ContentPage
+{
+	public Producto()
+	{
+		InitializeComponent();
+	}
+}
--- P7 Analisis03/producto.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace P7 Analisis03;
-
-public partial class producto : ContentPage
-{
-	public producto()
-	{
-		InitializeComponent();
-	}
-}
